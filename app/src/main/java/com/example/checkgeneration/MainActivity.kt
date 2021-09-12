746dd8d52f66878d91d8ac924df602364cc15c0b
lealpy <lealp@mail.ru>
2021-09-12 15:50:05 +0300

    Generations App

--- app/src/main/java/com/example/checkgeneration/MainActivity.kt
+++ app/src/main/java/com/example/checkgeneration/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.DatePicker
 import com.example.checkgeneration.databinding.ActivityMainBinding
